d5fc552faeb94ca3bbf18a8bb75e23d3f0128e33
pratu13 <41483788+pratu13@users.noreply.github.com>
2021-10-05 16:51:47 -0400

    first commit

---
+++ Mindfulness/Mindfulness/MindfulnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindfulnessApp.swift
+//  Mindfulness
+//
+//  Created by Pratyush  on 10/5/21.
+//
+
+import SwiftUI
+
+@main
+struct MindfulnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
